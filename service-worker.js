7d7682e5ba223eda502d1e4b70691d4e9857eb4f
rishabhkesarwani <rishabh.e203@gmail.com>
2020-04-28 19:10:32 +0000

    Deploying to gh-pages from  @ fccb9db59aa318c6123b064a60fa545e604a99af 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64c1f9406f13ccf76b4ad42fad3ffa06.js"
+  "/precache-manifest.9e079e2e8e27de183059457b478e2c02.js"
 );
 
 self.addEventListener('message', (event) => {
